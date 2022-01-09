0b0e4b0bc6da459a2c4f1a528b2e998313f871c5
w3tsa <tanveersayem@gmail.com>
2022-01-09 15:40:33 -0500

    :tada: profile is done

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,35 +1,112 @@
+import { useState, useEffect } from "react";
+import {
+  ref,
+  getDownloadURL,
+  uploadBytes,
+  deleteObject,
+} from "firebase/storage";
+import { getDoc, doc, updateDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
+
 /* local import */
 import Images from "../assets/Images";
 import Camera from "../components/svg/Camera";
+import Delete from "../components/svg/Delete";
+import { storage, db, auth } from "../firebase";
 
 export default function Profile() {
+  const [img, setImg] = useState("");
+  const [user, setUser] = useState();
+
+  function handleChange(e) {
+    setImg(e.target.files[0]);
+  }
+
+  async function deleteImage() {
+    try {
+      const confirm = window.confirm("Delete avatar?");
+      if (confirm) {
+        await deleteObject(ref(storage, user.avatarPath));
+
+        await updateDoc(doc(db, "users", auth.currentUser.uid), {
+          avatar: "",
+          avatarPath: "",
+        });
+
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getDoc(doc(db, "users", auth.currentUser.uid)).then((docSnap) => {
+      if (docSnap.exists) {
+        setUser(docSnap.data());
+      }
+    });
+    if (img) {
+      async function uploading() {
+        const imgRef = ref(
+          storage,
+          `avatar/${new Date().getTime()} - ${img.name}`
+        );
+        try {
+          if (user && user.avatarPath) {
+            await deleteObject(ref(storage, user.avatarPath));
+          }
+          const snap = await uploadBytes(imgRef, img);
+          const url = await getDownloadURL(ref(storage, snap.ref.fullPath));
+
+          await updateDoc(doc(db, "users", auth.currentUser.uid), {
+            avatar: url,
+            avatarPath: snap.ref.fullPath,
+          });
+          setImg("");
+        } catch (error) {
+          console.log(error.message);
+        }
+      }
+
+      uploading();
+    }
+  }, [img]);
+
+  const defaultAvatar = Images[Math.floor(Math.random() * Images.length)];
+
   return (
     <section>
       <div className="profile_container">
         <div className="img_container">
-          <img
-            src={Images[Math.floor(Math.random() * Images.length)]}
-            alt="avatar"
-          />
+          <img src={(user && user.avatar) || defaultAvatar} alt="avatar" />
           <div className="overlay">
             <div>
               <label htmlFor="photo">
                 <Camera />
               </label>
+              {user && user.avatar ? (
+                <Delete deleteImage={deleteImage} />
+              ) : null}
               <input
                 type="file"
                 accept="image/*"
                 style={{ display: "none" }}
                 id="photo"
+                onChange={handleChange}
               />
             </div>
           </div>
         </div>
         <div className="text_container">
-          <h3>User Name</h3>
-          <p>User email</p>
+          <h3>{user && user.name}</h3>
+          <p>{user && user.email}</p>
           <hr />
-          <small>Joined on: ....</small>
+          <small>
+            Joined on: {user && user.createdAt.toDate().toDateString()}
+          </small>
         </div>
       </div>
     </section>
