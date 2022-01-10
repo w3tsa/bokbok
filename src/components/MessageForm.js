55898fc64278e5665d60397707d1154c8aee893f
w3tsa <tanveersayem@gmail.com>
2022-01-09 20:51:00 -0500

    added send styles to message form

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -1,7 +1,26 @@
+/* local import */
+import Attachment from "./svg/Attachment";
+import Send from "./svg/Send";
 function MessageForm() {
   return (
     <form className="message_form">
-      <label htmlFor="img"></label>
+      <label htmlFor="img">
+        <Attachment />
+      </label>
+      <input
+        type="file"
+        id="img"
+        accept="image/*"
+        style={{ display: "none" }}
+      />
+      <div>
+        <input type="text" placeholder="Enter message" />
+      </div>
+      <div>
+        <button className="send">
+          <Send />
+        </button>
+      </div>
     </form>
   );
 }
