4005eb2df48e08cd514f00194b2893d11e611103
w3tsa <tanveersayem@gmail.com>
2022-01-14 19:40:00 -0500

    bug

--- src/components/User.js
+++ src/components/User.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react/cjs/react.development";
+import { useEffect, useState } from "react";
 import { onSnapshot, doc } from "firebase/firestore";
 import { db } from "../firebase";
 
